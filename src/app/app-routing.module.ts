105e0288f9c86fa2b360ae7b1180d8b4490a9787
aomar <aftis.aomar@gmail.com>
2023-04-04 02:27:44 +0200

    init commit inluding routing and creation of component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'tutorials', component: TutorialsListComponent},
+{path : 'add', component : AddTutorialComponent},
+{path : 'tutorial/:id', component : TutorialDetailsComponent},
+{path : '', redirectTo : 'tutorials', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
